4f055d481fbc233cc77741aaa261bacb798ea29d
brianlam38 <brian.lam38@gmail.com>
2016-04-17 17:47:55 +1000

    committing all files

--- mandelbrotcolours.c
+++ mandelbrotcolours.c
@@ -1,36 +1,78 @@
 /*
 
-I have defined a total of 7 ranges of colors.
-3 of them are in the red color zone, i.e., in the RGB space, R moves from 0 to 255 and G and B are constantly 0.
-The next 4 ranges are in the orange to yellow color zone, i.e., R is constantly 255, G moves from 0 to 255 and B is constantly 0.
-My current algorithm assigns linearly colors within each range, depending on the number of iterations required to decide if a pixel belongs or not in the set.
-The pseudo-code to assign a color to each pixel 'i' looks like this:
+(1) I have defined a total of 7 ranges of COLOURS for 7 sections of STEPS
+(2) FIRST 3 = of them are in the red colour zone
+    i.e., in the RGB space, R moves from 0 to 255 and G and B are constantly 0.
+(3) NEXT 4 = are in the orange to yellow colour zone
+    i.e., R is constantly 255, G moves from 0 to 255 and B is constantly 0.
+(4) This algorithm assigns linearly colors within each range, depending on the number of iterations required
+    to decide if a pixel belongs or not in the set.
 
 */
 
-maxIterations = 256;
+// Using RGB ranges (0-255,0-255,0) to create a fiery red and orange colour.
 
-while (iterationsPerPixel < 64) {
-	/
+#include <stdio.h>
+#include <stdlib.h>
+#include "pixelColor.h"
+#include "mandelbrot.h"
+
+#define STEPS_SECTION_1 37
+#define STEPS_SECTION_2 74
+#define STEPS_SECTION_3 111
+#define STEPS_SECTION_4 148
+#define STEPS_SECTION_5 185
+#define STEPS_SECTION_6 222
+#define STEPS_SECTION_7 256 // Maximum iteration
+
+unsigned char stepsToRed (int steps) {
+    unsigned char intensity;
+    if (steps <= STEPS_SECTION_1) {             /* RED SHADE 1 */
+        intensity = ((steps * 2) + 50));
+    } else if (steps <= STEPS_SECTION_2) {      /* RED SHADE 2 */
+        intensity = ((steps * 1.66) + 75));
+    } else if (steps <= STEPS_SECTION_3) {      /* RED SHADE 3 */
+        intensity = ((steps * 1.33) + 100));
+    } else if (steps <= STEPS_SECTION_4) {      /* ORANGE TO YELLOW 1 */
+        intensity = 255;
+    } else if (steps <= STEPS_SECTION_5) {      /* ORANGE TO YELLOW 2 */
+        intensity = 255;
+    } else if (steps <= STEPS_SECTION_6) {      /* ORANGE TO YELLOW 3 */
+        intensity = 255;
+    } else if (steps < STEPS_SECTION_7) {       /* ORANGE TO YELLOW 4 */
+        intensity = 255;
+    } else if (steps == STEPS_SECTION_7 {       /* In set. Assign black */
+        intensity = 0;
+    }
+    return intensity;
 }
 
+unsigned char stepsToGreen (int steps) {
+    unsigned char intensity;
+    if (steps <= STEPS_SECTION_1) {             /* RED SHADE 1 */
+        intensity = 0;
+    } else if (steps <= STEPS_SECTION_2) {      /* RED SHADE 2 */
+        intensity = 0;
+    } else if (steps <= STEPS_SECTION_3) {      /* RED SHADE 3 */
+        intensity = 0);
+    } else if (steps <= STEPS_SECTION_4) {      /* ORANGE TO YELLOW 1 */
+        intensity = ((steps * 1.25) + 30);
+    } else if (steps <= STEPS_SECTION_5) {      /* ORANGE TO YELLOW 2 */
+        intensity = ((steps * 1.12) + 60);
+    } else if (steps <= STEPS_SECTION_6) {      /* ORANGE TO YELLOW 3 */
+        intensity = ((steps * 1.06) + 90);
+    } else if (steps < STEPS_SECTION_7) {       /* ORANGE TO YELLOW 4 */
+        intensity = ((steps * 1.03) - 10);
+    } else if (steps == STEPS_SECTION_7) {      /* In set. Assign black */
+        intensity = 0;
+    }
+    return intensity;
+}
 
-if (IterationsPerPixel != MaxIterations) {
-  (R,G,B) = (0, 0, 0);  /* In the set. Assign black. */
-} else if (IterationsPerPixel < 64) {
-  (R,G,B) = (IterationsPerPixel * 2, 0, 0);    /* 0x0000 to 0x007E */
-} else if (IterationsPerPixel < 128) {
-  (R,G,B) = ((((IterationsPerPixel - 64) * 128) / 126) + 128, 0, 0);    /* 0x0080 to 0x00C0 */
-} else if (IterationsPerPixel < 256) {
-  (R,G,B) = ((((IterationsPerPixel - 128) * 62) / 127) + 193, 0, 0);    /* 0x00C1 to 0x00FF */
-} else if (IterationsPerPixel < 512) {
-  (R,G,B) = (255, (((IterationsPerPixel - 256) * 62) / 255) + 1, 0);    /* 0x01FF to 0x3FFF */
-} else if (IterationsPerPixel < 1024) {
-  (R,G,B) = (255, (((IterationsPerPixel - 512) * 63) / 511) + 64, 0);   /* 0x40FF to 0x7FFF */
-} else if (IterationsPerPixel < 2048) {
-  (R,G,B) = (255, (((IterationsPerPixel - 1024) * 63) / 1023) + 128, 0);   /* 0x80FF to 0xBFFF */
-} else if (IterationsPerPixel < 4096) {
-  (R,G,B) = (255, (((IterationsPerPixel - 2048) * 63) / 2047) + 192, 0);   /* 0xC0FF to 0xFFFF */
-} else {
-  (R, G, b) = (255, 255, 0);
+unsigned char stepsToBlue (int steps) {
+    unsigned char intensity;
+    if (steps <= STEPS_SECTION_7) {
+        intensity = 0;
+    }
+    return intensity;
 }
